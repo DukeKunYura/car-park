69b3cc83b81a367bbfaec983ccee9e227f48b60c
duke <dukekunyura@gmail.ru>
2023-06-14 22:40:18 +0300

    [*] create repository and add dbchangelog

--- src/main/java/com/duke/carpark/CarParkApplication.java
+++ src/main/java/com/duke/carpark/CarParkApplication.java
@@ -1,5 +1,6 @@
 package com.duke.carpark;
 
+import org.hibernate.boot.SchemaAutoTooling;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,7 @@ public class CarParkApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CarParkApplication.class, args);
+		System.out.println("Hey");
 	}
 
 }
