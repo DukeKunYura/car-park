b303273adc1ae8cdb8e18b12b3a466f5fa3cb932
duke <dukekunyura@gmail.ru>
2023-06-14 22:29:41 +0300

    init

---
+++ src/test/java/com/duke/carpark/CarParkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duke.carpark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarParkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
