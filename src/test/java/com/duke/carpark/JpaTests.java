183bdfb86dc978d13d66410cc45283fecc710dd0
dukekunyura <dukekunyura@gmail.com>
2023-06-18 12:44:42 +0300

    [*] add detail and account in test

--- src/test/java/com/duke/carpark/JpaTests.java
+++ src/test/java/com/duke/carpark/JpaTests.java
@@ -1,8 +1,11 @@
 package com.duke.carpark;
 
+import com.duke.carpark.entity.Account;
 import com.duke.carpark.entity.Car;
+import com.duke.carpark.entity.Detail;
 import com.duke.carpark.entity.Person;
 import com.duke.carpark.repository.CarRepository;
+import com.duke.carpark.repository.DetailRepository;
 import com.duke.carpark.repository.PersonRepository;
 import jakarta.annotation.PostConstruct;
 import org.junit.jupiter.api.Test;
@@ -10,6 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.annotation.Rollback;
 
+import java.math.BigDecimal;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -20,37 +24,64 @@ public class JpaTests {
     PersonRepository personRepository;
     @Autowired
     CarRepository carRepository;
+    @Autowired
+    DetailRepository detailRepository;
     private UUID personId;
+    private UUID carId;
 
     @PostConstruct
-    void addThreePersonalCar() {
+    void addPersonalCarsAndDetails() {
         Person person = new Person();
-        person.setPassportNumber("202020");
-        person.setFirstName("Gustav");
-        person.setSurname("Klimt");
+        person.setPassportNumber("808080");
+        person.setFirstName("Claude");
+        person.setSurname("Monet");
         person = personRepository.save(person);
         personId = person.getId();
 
+        Car renault = new Car();
+        renault.setBrand("Renault");
+        renault.setNumber("8888");
+        renault.setVin("9111");
+        renault = carRepository.save(renault);
+        carId = renault.getId();
+
+        Car chevrolet = new Car();
+        chevrolet.setBrand("Chevrolet");
+        chevrolet.setNumber("5050");
+        chevrolet.setVin("9222");
+        chevrolet = carRepository.save(chevrolet);
+
+        Detail engine = new Detail();
+        engine.setSerialNumber("E-001");
+        engine.setTitle("engine");
+        detailRepository.save(engine);
+
+        Detail battery = new Detail();
+        battery.setSerialNumber("B-002");
+        battery.setTitle("battery");
+        detailRepository.save(battery);
+
+        renault.addDetail(engine);
+        renault.addDetail(battery);
+        carRepository.save(renault);
+
+        person.addCar(chevrolet);
+        person.addCar(renault);
+        personRepository.save(person);
+
         {
-            Car car = new Car();
-            car.setBrand("Audi");
-            car.setNumber("8888");
-            car.setVin("9111");
-            person.addCar(car);
+            Account account = new Account();
+            account.setTitle("First account");
+            account.setNumber("ABCD-1");
+            account.setScore(new BigDecimal(0.01));
+            person.addAccount(account);
         }
         {
-            Car car = new Car();
-            car.setBrand("Mercedes");
-            car.setNumber("9000");
-            car.setVin("9333");
-            person.addCar(car);
-        }
-        {
-            Car car = new Car();
-            car.setBrand("Chevrolet");
-            car.setNumber("5050");
-            car.setVin("9222");
-            person.addCar(car);
+            Account account = new Account();
+            account.setTitle("Second account");
+            account.setNumber("ABCD-2");
+            account.setScore(new BigDecimal(10.00));
+            person.addAccount(account);
         }
         personRepository.save(person);
     }
@@ -59,8 +90,10 @@ public class JpaTests {
     @Rollback(value = false)
     void checkAddedCars() {
         Person person = personRepository.findById(personId).orElseThrow();
-        person.getCars().size();
-        System.out.println("person.getCars().size() = " + person.getCars().size());
-        assertEquals(3, person.getCars().size());
+        assertEquals(2, person.getCars().size());
+        assertEquals(2, person.getAccounts().size());
+        Car car = carRepository.findCarById(carId);
+        assertEquals(2, car.getDetails().size());
     }
+
 }
